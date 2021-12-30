88ceae0106b81de6454337c6dfff8af6d65e1ee8
Nathaniel M <nmoratto629@gmail.com>
2021-12-29 18:52:16 -0800

    Linux Support for uninject since I forgot this last time as well.

--- src/app/actions/uninject.js
+++ src/app/actions/uninject.js
@@ -1,5 +1,7 @@
 const { access, rm } = require("fs");
 const { exec } = require("child_process");
+const {join} = require("path");
+const sudo = require("sudo-prompt");
 
 module.exports = async() => {
     // Define Guilded Directory from Global Variable for easy access.
@@ -13,10 +15,20 @@ module.exports = async() => {
             await new Promise((resolve, reject) => {
 
                 // Removes the Guilded/Resources/App directory.
-                rm(guildedDir, {force: true, recursive: true}, async (err) => {
-                    if (err) throw reject(err);
-                    else resolve();
-                })
+                // Elevate for Linux and run Terminal Commands
+                // Or use FS for Windows & Mac
+                if (process.platform === "linux" && process.getuid() !== 0) {
+                    const command = `rm -rf ${guildedDir}`
+                    sudo.exec(command, {name: "ReGuilded Installer"}, function(err, stdout, stderr) {
+                        if (err) throw reject(err);
+                        else resolve();
+                    });
+                } else {
+                    rm(guildedDir, {force: true, recursive: true}, async (err) => {
+                        if (err) throw reject(err);
+                        else resolve();
+                    });
+                }
             })
 
             // Relaunch Guilded
