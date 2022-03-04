531bce94be075ced6a3f003c1c1ef4c7a570a5ac
Nathaniel M <nmoratto629@gmail.com>
2022-03-03 19:51:46 -0800

    Temp Inject & Uninject setTimeout.

--- src/app/actions/uninject.js
+++ src/app/actions/uninject.js
@@ -2,43 +2,51 @@ const { access, rm } = require("fs");
 const { exec } = require("child_process");
 const sudo = require("sudo-prompt");
 
-module.exports = async() => {
+module.exports = () => {
     return new Promise((resolve, reject) => {
-        // Define Guilded Directory from Global Variable for easy access.
-        const guildedDir = window.platform.dir;
-
-        // Double Check if ReGuilded is injected.
-        access(guildedDir, async(err) => {
-            if (!err) { // Is Injected
-
-                await new Promise((uninjectResolve) => {
-                    // Removes the Guilded/Resources/App directory.
-                    // Elevate for Linux and run Terminal Commands
-                    // Or use FS for Windows & Mac
-                    if (process.platform === "linux" && process.getuid() !== 0) {
-                        const command = `rm -rf ${guildedDir}`
-                        sudo.exec(command, {name: "ReGuilded Installer"}, function(err, stdout, stderr) {
-                            if (err) throw reject(err);
-                            else uninjectResolve();
-                        });
-                    } else {
-                        rm(guildedDir, {force: true, recursive: true}, async (err) => {
-                            if (err) throw reject(err);
-                            else uninjectResolve();
-                        });
-                    }
-                });
-
-                // Relaunch Guilded
-                exec(window.platform.close, () => {
-                    exec(window.platform.open(), (openError) => {
-                        if (openError)
-                            throw reject("E" + openError);
-                    });
-                });
-
-                resolve();
-            } else throw reject("INJECTION_NOT_FOUND");
-        });
+        setTimeout(() => {
+            resolve(["update", "inject"]);
+        }, 2000)
     });
 }
+
+// module.exports = async() => {
+//     return new Promise((resolve, reject) => {
+//         // Define Guilded Directory from Global Variable for easy access.
+//         const guildedDir = window.platform.dir;
+//
+//         // Double Check if ReGuilded is injected.
+//         access(guildedDir, async(err) => {
+//             if (!err) { // Is Injected
+//
+//                 await new Promise((uninjectResolve) => {
+//                     // Removes the Guilded/Resources/App directory.
+//                     // Elevate for Linux and run Terminal Commands
+//                     // Or use FS for Windows & Mac
+//                     if (process.platform === "linux" && process.getuid() !== 0) {
+//                         const command = `rm -rf ${guildedDir}`
+//                         sudo.exec(command, {name: "ReGuilded Installer"}, function(err, stdout, stderr) {
+//                             if (err) throw reject(err);
+//                             else uninjectResolve();
+//                         });
+//                     } else {
+//                         rm(guildedDir, {force: true, recursive: true}, async (err) => {
+//                             if (err) throw reject(err);
+//                             else uninjectResolve();
+//                         });
+//                     }
+//                 });
+//
+//                 // Relaunch Guilded
+//                 exec(window.platform.close, () => {
+//                     exec(window.platform.open(), (openError) => {
+//                         if (openError)
+//                             throw reject("E" + openError);
+//                     });
+//                 });
+//
+//                 resolve();
+//             } else throw reject("INJECTION_NOT_FOUND");
+//         });
+//     });
+// }
