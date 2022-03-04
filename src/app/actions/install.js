07801210f98f722a575626715861da6410509814
Nathaniel M <nmoratto629@gmail.com>
2022-03-03 19:52:24 -0800

    Install logic.
    This is 100% tested working on Windows, but not on linux & mac.
    - Also can't inject yet, so this is useless.

--- src/app/actions/install.js
+++ src/app/actions/install.js
@@ -1,61 +1,102 @@
 // Custom Modules
-const gitHandler = require("../util").gitHandler;
+const { isInjected } = require("../util")
 
 // NPM Modules
-const { Extract } = require("unzipper");
-const { copy } = require("fs-extra");
+const { createWriteStream } = require("fs-extra");
+const sudo = require("sudo-prompt");
 const { stream } = require("got");
 const { join } = require("path");
-const fs = require("fs");
 
-module.exports = async (devBuild) => {
+module.exports = () => {
     return new Promise(async (resolve, reject) => {
-        const commit = gitHandler.getCommit(devBuild);
-        const reguildedDir = join(process.env.APPDATA ?? process.env.HOME, ".reguilded");
-        const zipPath = reguildedDir + ".zip";
+        process.noAsar = true;
+        const downloadUrl = window.gitRelease.downloadUrl
+        const downloadPath = join(__dirname, "reguilded.asar");
 
-        // Download Zip & Extract Zip
-        await new Promise((zipResolve) => {
-            stream(commit.zipUrl)
-                .pipe(fs.createWriteStream(zipPath))
-                .on("finish", function() {
-                    fs.createReadStream(zipPath)
-                        .pipe(Extract({ path: reguildedDir })).on("close", function () {
-                            fs.unlink(zipPath, (err) => {
-                                if (err) reject(err);
-                                else zipResolve();
-                            });
-                        });
-                });
+        await new Promise((downloadResolve) => {
+            try {
+                stream(downloadUrl)
+                    .pipe(createWriteStream(downloadPath))
+                    .on("finish", async () => {
+                        downloadResolve()
+                    });
+            } catch (err) {
+                reject(err);
+            }
         });
 
-        // Move downloaded files into `~/.reguilded` and then delete the old folder.
+        let command = ""
+        if (["linux", "darwin"].includes(process.platform)) {
+            command = `mv -f ${JSON.stringify(downloadUrl)} ${JSON.stringify(join(window.platform.reguildedDir, "reguilded.asar"))} & ` +
+                `chmod -R 777 ${JSON.stringify(window.platform.reguildedDir)}`
+        } else if (["win32"].includes(process.platform)) {
+            command = `mkdir ${JSON.stringify(window.platform.reguildedDir)} 2>nul & ` +
+                `move /Y ${JSON.stringify(downloadPath)} ${JSON.stringify(join(window.platform.reguildedDir, "reguilded.asar"))} & ` +
+                `icacls ${JSON.stringify(window.platform.reguildedDir)} /grant "Authenticated Users":(OI)(CI)F`
+        }
+
         await new Promise((moveResolve) => {
-            fs.readdir(reguildedDir, {withFileTypes: true}, (err, files) => {
-                const dirs = files.filter(file => file.isDirectory());
-                const gitDir = join(reguildedDir, dirs[0].name);
-                const appDir = join(gitDir, "src/app");
+            sudo.exec(command, {name: "ReGuilded Installer"}, (err, stdout, stderr) => {
+                if (err) reject(err);
+                if (stderr) console.log(stderr);
 
-                copy(appDir, reguildedDir, { recursive: true, errorOnExist: false, overwrite: true}, (err) => {
-                    if (err) throw err;
-                    else {
-                        fs.rmdir(gitDir, {recursive: true}, (err) => {
-                            if (err) reject(err);
-                            else moveResolve();
-                        })
-                    }
-                })
+                moveResolve();
             });
         });
 
-        // Create new `.sha` file inside `~/.reguilded` that has the commitSha used for checking latest version.
-        await new Promise((shaResolve) => {
-            fs.writeFile(join(reguildedDir, ".sha"), commit.commitSha, (err) => {
-                if (err) reject(err);
-                else shaResolve();
-            });
-        })
-
-        resolve();
+        process.noAsar = false;
+        resolve(["update", await isInjected() ? "uninject" : "inject"]);
     });
 }
+
+// module.exports = async (devBuild) => {
+//     return new Promise(async (resolve, reject) => {
+//         const commit = gitHandler.getCommit(devBuild);
+//         const reguildedDir = join(process.env.APPDATA ?? process.env.HOME, ".reguilded");
+//         const zipPath = reguildedDir + ".zip";
+//
+//         // Download Zip & Extract Zip
+//         await new Promise((zipResolve) => {
+//             stream(commit.zipUrl)
+//                 .pipe(fs.createWriteStream(zipPath))
+//                 .on("finish", function() {
+//                     fs.createReadStream(zipPath)
+//                         .pipe(Extract({ path: reguildedDir })).on("close", function () {
+//                             fs.unlink(zipPath, (err) => {
+//                                 if (err) reject(err);
+//                                 else zipResolve();
+//                             });
+//                         });
+//                 });
+//         });
+//
+//         // Move downloaded files into `~/.reguilded` and then delete the old folder.
+//         await new Promise((moveResolve) => {
+//             fs.readdir(reguildedDir, {withFileTypes: true}, (err, files) => {
+//                 const dirs = files.filter(file => file.isDirectory());
+//                 const gitDir = join(reguildedDir, dirs[0].name);
+//                 const appDir = join(gitDir, "src/app");
+//
+//                 copy(appDir, reguildedDir, { recursive: true, errorOnExist: false, overwrite: true}, (err) => {
+//                     if (err) throw err;
+//                     else {
+//                         fs.rmdir(gitDir, {recursive: true}, (err) => {
+//                             if (err) reject(err);
+//                             else moveResolve();
+//                         })
+//                     }
+//                 })
+//             });
+//         });
+//
+//         // Create new `.sha` file inside `~/.reguilded` that has the commitSha used for checking latest version.
+//         await new Promise((shaResolve) => {
+//             fs.writeFile(join(reguildedDir, ".sha"), commit.commitSha, (err) => {
+//                 if (err) reject(err);
+//                 else shaResolve();
+//             });
+//         })
+//
+//         resolve();
+//     });
+// }
