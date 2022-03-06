72bfa153e0a3dd98d955edd93d03fa99b4f77c05
Nathaniel M <nmoratto629@gmail.com>
2022-03-05 18:42:59 -0800

    2/2 Injection & Overall code improvements.
    - Injection now completed for Windows.
    - Injection not tested for Mac.
    - Realized the promises never stopped, so added return process along with the main reject.

--- src/app/actions/inject.js
+++ src/app/actions/inject.js
@@ -1,8 +1,7 @@
-const { writeFile, access, mkdir } = require("fs");
-const { exec } = require("child_process");
+const { writeFile, access, mkdir, rename } = require("fs");
+const { isInjected } = require("../util");
 const { join, sep } = require("path");
 const sudo = require("sudo-prompt");
-const { isInjected } = require("../util");
 
 module.exports = () => {
     return new Promise((resolve, reject) => {
@@ -19,23 +18,55 @@ module.exports = () => {
                     if (["linux"].includes(process.platform)) {
                         const command = `mkdir ${JSON.stringify(window.platform.appDir)} && ` +
                             `echo '{"name": "Guilded", "main": "index.js"}' | tee -a ${JSON.stringify(join(window.platform.appDir, "package.json"))} > /dev/null && ` +
-                            `echo 'require(${JSON.stringify(reguildedAsarPath)})' | tee -a ${JSON.stringify(join(window.platform.appDir, "index.js"))} > /dev/null && ` +
+                            `echo 'require(${JSON.stringify(reguildedAsarPath)});' | tee -a ${JSON.stringify(join(window.platform.appDir, "index.js"))} > /dev/null && ` +
                             `mkdir ${JSON.stringify(guildedMoveToPath)} && ` +
                             `mv -f ${JSON.stringify(join(window.platform.resourcesDir, "app.asar"))} ${JSON.stringify(join(guildedMoveToPath, "app.asar"))} && ` +
                             `mv -f ${JSON.stringify(join(window.platform.resourcesDir, "app.asar.unpacked"))} ${JSON.stringify(join(guildedMoveToPath, "app.asar.unpacked"))}`
 
                         sudo.exec(command, {name: "ReGuilded Installer"}, async (err, stdout, stderr) => {
-                            if (err) reject(err);
-                            if (stderr) reject(stderr);
+                            if (err) { reject(err); return; }
+                            if (stderr) { reject(stderr); return; }
 
                             // Even though this is still a return if inject is successful,
                             // a double check to make sure that ReGuilded is indeed injected.
                             resolve(["update", await isInjected() ? "uninject" : "inject"]);
                         });
                     } else {
-                        await new Promise((guildedMoveResolve) => {
+                        await new Promise((reguildedAppDirResolve) => {
+                            mkdir(window.platform.appDir, (mkDirErr) => {
+                                if (mkDirErr) { reject(mkDirErr);  return; }
 
-                        })
+                                writeFile(join(window.platform.appDir, "index.js"), `require(${JSON.stringify(reguildedAsarPath)});`, (jsWriteErr) => {
+                                    if (jsWriteErr) { reject(jsWriteErr); return; }
+
+                                    writeFile(join(window.platform.appDir, "package.json"), JSON.stringify({ name: "Guilded", main: "index.js" }), (jsonWriteErr) => {
+                                        if (jsonWriteErr) { reject(jsonWriteErr); return; }
+
+                                        reguildedAppDirResolve();
+                                    });
+                                });
+                            });
+                        });
+
+                        await new Promise((guildedMoveResolve) => {
+                            mkdir(guildedMoveToPath, (mkDirErr) => {
+                                if (mkDirErr) { reject(mkDirErr); return; }
+
+                                rename(join(window.platform.resourcesDir, "app.asar"), join(guildedMoveToPath, "app.asar"), (asarRenameErr) => {
+                                    if (asarRenameErr) { reject(asarRenameErr); return; }
+
+                                    rename(join(window.platform.resourcesDir, "app.asar.unpacked"), join(guildedMoveToPath, "app.asar.unpacked"), (asarUnpackedRenameErr) => {
+                                        if (asarUnpackedRenameErr) { reject(asarUnpackedRenameErr); return; }
+
+                                        guildedMoveResolve();
+                                    });
+                                });
+                            });
+                        });
+
+                        // Even though this is still a return if inject is successful,
+                        // a double check to make sure that ReGuilded is indeed injected.
+                        resolve(["update", await isInjected() ? "uninject" : "inject"]);
                     }
                 } catch (err) {
                     reject(err);
