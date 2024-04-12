7da7b239527cf85f4d46aa17afba955ddca4c294
Nathaniel M <me@itznxthaniel.dev>
2024-04-11 21:39:21 -0400

    Load GH Releases and work on Install screen.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs::File;
+use std::io::Write;
 use std::path::Path;
-use tauri::{Manager};
+use reqwest::blocking::Client;
+use reqwest::header;
+use serde::{Deserialize, Serialize};
+use tauri::{AppHandle, Manager};
 use crate::util::platform_handler::PlatformHandler;
 
 mod util { pub mod platform_handler; }
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Asset {
+    name: String,
+    browser_download_url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Release {
+    name: String,
+    tag_name: String,
+    draft: bool,
+    prerelease: bool,
+    assets: Vec<Asset>
+}
 
 #[tauri::command]
 fn preface_begin(window: tauri::Window) {
@@ -29,6 +49,44 @@ fn get_rg_install() -> Result<(), String> {
     }
 }
 
+
+#[tauri::command]
+fn get_rg_path() -> String {
+    let platform_handler = &*PLATFORM_HANDLER.lock().unwrap();
+
+    let platform = platform_handler.get_platform();
+
+    platform.unwrap().reguilded_dir.to_string_lossy().to_string().into()
+}
+
+fn fetch_releases_and_save(app_handle: AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+
+    let request = client.get("https://api.github.com/repos/reguilded/reguilded/releases")
+        .header(header::USER_AGENT, format!("ReGuilded-Installer/{}", env!("CARGO_PKG_VERSION")));
+
+    let response = request.send()?;
+
+    if response.status().is_success() {
+        let releases: Vec<Release> = response.json()?;
+
+        let resource_path = app_handle.path_resolver()
+            .resolve_resource("resources/releases.json")
+            .expect("Failed to resolve resource.");
+
+
+        let mut file = File::create(&resource_path)?;
+        file.write_all(serde_json::to_string_pretty(&releases).unwrap().as_bytes()).unwrap();
+
+        Ok(())
+    } else {
+        let status = response.status();
+        let body = response.text()?;
+        eprintln!("Failed to fetch releases. Status code: {}. Body: {}", status, body);
+        Err("Failed to fetch releases".into())
+    }
+}
+
 lazy_static::lazy_static! {
     static ref PLATFORM_HANDLER: std::sync::Mutex<PlatformHandler> = std::sync::Mutex::new(PlatformHandler::new());
 }
@@ -39,21 +97,24 @@ fn main() {
             let splashscreen_window = app.get_window("splashscreen").unwrap();
             let preface_window = app.get_window("preface").unwrap();
 
-            tauri::async_runtime::spawn(async move {
-                println!("Initializing...");
-                std::thread::sleep(std::time::Duration::from_secs(2));
-                println!("Done initializing...");
+            if let Err(err) = fetch_releases_and_save(app.handle()) {
+                eprintln!("There was an error: {}", err);
 
-                splashscreen_window.close().unwrap();
-                preface_window.show().unwrap();
-                preface_window.set_focus().unwrap();
-            });
+                // TODO: SHOW ERROR SCREEN!
+
+                return Err("fetch_releases_and_save errored!".into());
+            }
+
+            splashscreen_window.close().unwrap();
+            preface_window.show().unwrap();
+            preface_window.set_focus().unwrap();
 
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
             preface_begin,
-            get_rg_install
+            get_rg_install,
+            get_rg_path
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -1,28 +1,38 @@
 /* Init Handler */
 invoke("get_rg_install")
     .then(() => {
+        getRgPath();
         toggleElement(window.utilScreen, true)
     })
-    .catch((error) => {
+    .catch((error, defaultRgPath) => {
         switch (error) {
             case "NOT_SUPPORTED":
                 window.errorScreenText.innerHTML = "Uh Oh! It looks like your<br />Operating System is unsupported!"
                 toggleElement(window.errorScreen, true)
                 return;
             case "NO_INSTALL":
+                getRgPath();
                 toggleElement(window.errorScreen, true)
                 return;
         }
     })
 
+let reguildedPath;
+function getRgPath() {
+    invoke("get_rg_path").then((defaultRgPath) => {
+        reguildedPath = defaultRgPath;
+        window.locationText.innerText = reguildedPath;
+    })
+}
+
 /* Element Handler */
-function toggleElement(screen, isShown) {
+function toggleElement(element, isShown) {
     switch (isShown) {
         case true:
-            screen.classList.remove("hidden");
+            element.classList.remove("hidden");
             return;
         case false:
-            screen.classList.add("hidden");
+            element.classList.add("hidden");
             return;
     }
 }
@@ -32,4 +42,92 @@ window.newInstallText.addEventListener("click", () => {
     toggleElement(window.errorScreen, false)
     toggleElement(window.newInstallScreen, true)
     toggleElement(window.installButton, true);
+})
+
+/* Install Screen Handling */
+const { resolveResource } = window.__TAURI__.path;
+const { readTextFile } = window.__TAURI__.fs;
+const { open } = window.__TAURI__.dialog;
+let currentChannel = "stable";
+
+window.releaseChannel.append(new Option("Stable", "stable", true));
+window.releaseChannel.append(new Option("Legacy", "legacy"));
+
+(async() => {
+    const releasesPath = await resolveResource("resources/releases.json");
+    const releases = JSON.parse(await readTextFile(releasesPath));
+
+    releases.forEach((release) => {
+        const option = new Option(release.tag_name, release.tag_name);
+        const releaseName = release.name.toLowerCase();
+
+        if (!releaseName.includes("[legacy]") &&
+            !releaseName.includes("[broken]") &&
+            !releaseName.includes("[legacy/broken]")
+        ) {
+            option.setAttribute("data-value", "stable")
+        } else {
+            option.setAttribute("data-value", "legacy")
+            option.classList.add("hidden");
+        }
+
+        window.releaseVersion.append(option)
+    })
+})();
+
+const versionElements = window.releaseVersion.getElementsByTagName("option");
+
+function toggleVersions(channelString, isShown) {
+    let defaultVersionHandled = false;
+
+    for (let versionElement of versionElements) {
+        let dataValue = versionElement.getAttribute("data-value");
+        if (dataValue === channelString) {
+            toggleElement(versionElement, isShown);
+
+            if (isShown && !defaultVersionHandled) {
+                versionElement.selected = true;
+                defaultVersionHandled = true;
+            }
+        }
+    }
+}
+
+window.releaseChannel.addEventListener("change", () => {
+    switch (window.releaseChannel.value) {
+        case "stable":
+            toggleVersions(currentChannel, false);
+            toggleVersions("stable", true);
+
+            currentChannel = "stable";
+            return;
+        case "dev":
+            toggleVersions(false);
+            toggleVersions("dev", true);
+
+            currentChannel = "dev";
+            return;
+        case "legacy":
+            toggleVersions(currentChannel, false);
+            toggleVersions("legacy", true);
+
+            currentChannel = "legacy";
+            return;
+        default:
+            console.error("How did we get here.");
+            return;
+    }
+})
+
+window.locationContainer.addEventListener("click", async () => {
+    const selected = await open({
+        directory: true,
+        multiple: false,
+        defaultPath: reguildedPath
+    })
+
+    if (selected !== null) {
+        window.locationText.innerText = selected;
+        reguildedPath = selected;
+    }
 })
